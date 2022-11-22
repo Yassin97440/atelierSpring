20740d1dc8e3f344c3a272b7b35dce3b675e1a56
Yassin97440 <yassin.abdulla@compufirst.com>
2022-11-22 02:52:52 +0100

    add database connection + first model + repo & controller

---
+++ src/main/java/io/atelier/spring/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package io.atelier.spring.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import io.atelier.spring.model.User;
+
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
