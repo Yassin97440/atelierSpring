29f1e8171a921d152f8d5de0deecaf0d720d1e81
Yassin <yassin.abdulla@compufirst.com>
2022-11-26 19:17:55 +0100

    add cors (exemple, faut le config

--- src/main/java/io/atelier/spring/Application.java
+++ src/main/java/io/atelier/spring/Application.java
@@ -2,6 +2,9 @@ package io.atelier.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
@@ -9,5 +12,16 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	 @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "PUT", "POST", "DELETE").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
+
 
 }
